07aaa194b06a2515a7a1c8be496b4ebfafb76c7d
Sam Stoelinga <sammiestoel@gmail.com>
2023-07-24 14:42:07 -0700

    add ability to provide prompt template (#10)

--- model-trainer-huggingface/bin/notebook.sh
+++ model-trainer-huggingface/bin/notebook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-set -xe
+NOTEBOOK_TOKEN="${NOTEBOOK_TOKEN:-default}"
 
-jupyter lab --allow-root --ip=0.0.0.0 --NotebookApp.token='' --notebook-dir='/content'
+jupyter lab --allow-root --ip=0.0.0.0 --NotebookApp.token=$NOTEBOOK_TOKEN --notebook-dir=/content
