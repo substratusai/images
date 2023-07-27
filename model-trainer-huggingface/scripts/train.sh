ae5214d89bff51fbac48f9b9ae17d4558d211b7f
Sam Stoelinga <sammiestoel@gmail.com>
2023-07-26 22:02:35 -0700

    set token in HF images only

--- model-trainer-huggingface/scripts/train.sh
+++ model-trainer-huggingface/scripts/train.sh
@@ -3,5 +3,4 @@
 set -xe
 
 export HF_HUB_DISABLE_PROGRESS_BARS=1
-export HUGGING_FACE_HUB_TOKEN=$PARAM_HUGGING_FACE_HUB_TOKEN
 jupyter nbconvert --debug --to notebook --execute /content/src/train.ipynb --output /content/logs/train.ipynb
