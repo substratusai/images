15f8c8567b34edbc67941c3908f8d14e838613f8
Sam Stoelinga <sammiestoel@gmail.com>
2023-08-22 20:35:18 -0700

    make 4 bit mode configurable

--- model-server-basaran/scripts/serve.sh
+++ model-server-basaran/scripts/serve.sh
@@ -3,4 +3,14 @@
 set -xe
 
 export SERVER_MODEL_NAME="$HOSTNAME"
+export LOAD_IN_4BIT="${PARAM_LOAD_IN_4BIT:-false}"
+
+# Backwards compatiblity to keep load in 8 bit the default
+if [ "${LOAD_IN_4BIT}" = "false" ]; then
+echo "Loading model in 8 bit mode"
+export LOAD_IN_8BIT="true"
+else
+echo "Loading model in 4 bit mode"
+fi
+
 PYTHONUNBUFFERED=1 python -m basaran
