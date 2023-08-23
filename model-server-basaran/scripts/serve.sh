7134a5d973663c579673757f9a98a1527e144add
Sam Stoelinga <sammiestoel@gmail.com>
2023-08-22 21:36:49 -0700

    use correct env name

--- model-server-basaran/scripts/serve.sh
+++ model-server-basaran/scripts/serve.sh
@@ -3,12 +3,12 @@
 set -xe
 
 export SERVER_MODEL_NAME="$HOSTNAME"
-export LOAD_IN_4BIT="${PARAM_LOAD_IN_4BIT:-false}"
+export MODEL_LOAD_IN_4BIT="${PARAM_LOAD_IN_4BIT:-false}"
 
 # Backwards compatiblity to keep load in 8 bit the default
 if [ "${LOAD_IN_4BIT}" = "false" ]; then
 echo "Loading model in 8 bit mode"
-export LOAD_IN_8BIT="true"
+export MODEL_LOAD_IN_8BIT="true"
 else
 echo "Loading model in 4 bit mode"
 fi
