44204ce0f0e1839057cc3ce5a53ff11cbe065d8e
Sam Stoelinga <sammiestoel@gmail.com>
2023-09-09 15:16:35 -0700

    log output to stdout as well

--- base/scripts/run-notebook.sh
+++ base/scripts/run-notebook.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 set -xe
-
+papermill_flags=(--autosave-cell-every=1 --log-output --log-level DEBUG)
 if [[ -z "$NOTEBOOK" ]]; then
   for nb in /content/*.ipynb; do
     [ -e "$nb" ] || continue
-    papermill $nb /content/artifacts/$(basename "$nb") --autosave-cell-every=1
+    papermill $nb /content/artifacts/$(basename "$nb") "${papermill_flags[@]}"
     jupyter nbconvert --to html /content/artifacts/$(basename "$nb")
   done
   for nb in /content/src/*.ipynb; do
     [ -e "$nb" ] || continue
     mkdir -p /content/artifacts/src
-    papermill $nb /content/artifacts/src/$(basename "$nb") --autosave-cell-every=1
+    papermill $nb /content/artifacts/src/$(basename "$nb") "${papermill_flags[@]}"
     jupyter nbconvert --to html /content/artifacts/src/$(basename "$nb")
   done
 else
   for nb in $NOTEBOOK; do
     [ -e "$nb" ] || continue
-    papermill $nb /content/artifacts/$(basename "$nb") --autosave-cell-every=1
+    papermill $nb /content/artifacts/$(basename "$nb") "${papermill_flags[@]}"
     jupyter nbconvert --to html /content/artifacts/$(basename "$nb")
   done
 fi
