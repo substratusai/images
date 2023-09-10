f0ca9991ab5f080ea9d5d4be558ce5dc9b357a97
Sam Stoelinga <sammiestoel@gmail.com>
2023-09-10 10:44:15 -0700

    store scripts in /usr/local/bin

--- base/scripts/run-notebook.sh
+++ base/scripts/run-notebook.sh
@@ -4,19 +4,19 @@ set -xe
 if [[ -z "$NOTEBOOK" ]]; then
   for nb in /content/*.ipynb; do
     [ -e "$nb" ] || continue
-    python3 scripts/run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})"
+    run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})"
     # jupyter nbconvert --to html /content/artifacts/$(basename "$nb")
   done
   for nb in /content/src/*.ipynb; do
     [ -e "$nb" ] || continue
     mkdir -p /content/artifacts/src
-    python3 scripts/run-notebook.py "$nb" "/content/artifacts/src/$(basename ${nb})"
+    run-notebook.py "$nb" "/content/artifacts/src/$(basename ${nb})"
     # jupyter nbconvert --to html /content/artifacts//src/$(basename "$nb")
   done
 else
   for nb in $NOTEBOOK; do
     [ -e "$nb" ] || continue
-    python3 scripts/run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})"
+    run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})"
     # jupyter nbconvert --to html /content/artifacts/$(basename "$nb")
   done
 fi
