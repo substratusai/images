d13d2c2bc05e127443a4e9eb88922e6d3c367eb9
Sam Stoelinga <sammiestoel@gmail.com>
2023-10-01 00:00:12 -0700

    fix base image to support relative imports

--- base/scripts/run-notebook.py
+++ base/scripts/run-notebook.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
 from pathlib import Path
 import logging
@@ -8,6 +9,16 @@ from papermill.log import logger
 from papermill.engines import NBClientEngine, papermill_engines, NotebookExecutionManager, PapermillNotebookClient
 from papermill.utils import merge_kwargs, remove_args
 from nbconvert import HTMLExporter
+
+
+parser = argparse.ArgumentParser(
+    prog='run-notebook.py',
+    description='Run a notebook and converts it to HTML while it is running')
+
+parser.add_argument('notebook_path', help="The path to the .ipynb notebook file")
+parser.add_argument('notebook_output_path', help="The path where the .ipynb notebook file should be written")
+parser.add_argument('-c', '--cwd', help="Change the working directory")
+args = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger.setLevel(logging.INFO)
@@ -68,8 +79,9 @@ papermill_engines.register_entry_points()
 
 # Execute the notebook with papermill as usual
 papermill.execute_notebook(
-    sys.argv[1],
-    sys.argv[2],
+    args.notebook_path,
+    args.notebook_output_path,
+    cwd=args.cwd,
     log_output=True,
     engine_name="htmlengine",
 )
--- base/scripts/run-notebook.sh
+++ base/scripts/run-notebook.sh
@@ -5,21 +5,18 @@ if [[ -z "$NOTEBOOK" ]]; then
   for nb in /content/*.ipynb; do
     [ -e "$nb" ] || continue
     echo "Executing notebook $nb"
-    run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})"
-    # jupyter nbconvert --to html /content/artifacts/$(basename "$nb")
+    run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})" --cwd "$(dirname ${nb})"
   done
   for nb in /content/src/*.ipynb; do
     [ -e "$nb" ] || continue
     mkdir -p /content/artifacts/src
     echo "Executing notebook $nb"
-    run-notebook.py "$nb" "/content/artifacts/src/$(basename ${nb})"
-    # jupyter nbconvert --to html /content/artifacts//src/$(basename "$nb")
+    run-notebook.py "$nb" "/content/artifacts/src/$(basename ${nb})" --cwd "$(dirname ${nb})"
   done
 else
   for nb in $NOTEBOOK; do
     [ -e "$nb" ] || continue
     echo "Executing notebook $nb"
-    run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})"
-    # jupyter nbconvert --to html /content/artifacts/$(basename "$nb")
+    run-notebook.py "$nb" "/content/artifacts/$(basename ${nb})" --cwd "$(dirname ${nb})"
   done
 fi
