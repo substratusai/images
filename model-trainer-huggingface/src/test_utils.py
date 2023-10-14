4beaf23d69cac7750aa7b3c226b9dadb713e11b7
Sam Stoelinga <sammiestoel@gmail.com>
2023-10-13 20:12:18 -0700

    add support for configurable target_modules (#53)

--- model-trainer-huggingface/src/test_utils.py
+++ model-trainer-huggingface/src/test_utils.py
@@ -4,7 +4,7 @@ from .utils import parse_training_args
 
 
 def test_parse_training_args_int_float():
-    params = {"num_train_epochs": "1"}
+    params = {"num_train_epochs": "1", "target_modules": "q,v"}
     assert parse_training_args(params).num_train_epochs == 1.0
 
     params = {"num_train_epochs": "1", "max_steps": "5"}
